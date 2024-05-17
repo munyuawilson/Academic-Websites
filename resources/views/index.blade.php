d3160dc52f8c180749f064bfcdde2a10d5b8e664
Ivy Karunjigi <wmunyua4@gmail.com>
2024-05-17 14:56:05 +0300

    Getting started

---
+++ resources/views/index.blade.php
@@ -0,0 +1,110 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home|| Masters Assignment Help</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-light nav-background fixed-top">
+    <div class=" container-fluid mb-5 justify-content-center ">
+        <ul class="navbar-nav">
+            <li class="nav-item
+            ">+447554912584
+</li>
+            <li class="nav-item">+923436078969</li>
+            <li class="nav-item">info@masterassignmenthelp.com</li>
+            <li class="nav-item"><a href="">Order On Whatsapp</a></li>
+        </ul>
+    </div>
+    </nav>
+
+<nav class="navbar navbar-expand-md navbar-light nav-background fixed-top mt-5">
+            <a class="navbar-brand" href=""><img src="/static/logo.png" alt="" height="100px"><a>
+            <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center  " id="navbarNav">
+              <ul class="navbar-nav ">
+                <li class="nav-item link">
+                  <a class="nav-link  text-black" href="">Contact</a>
+                </li>
+                <li class="nav-item link">
+                  <a class="nav-link text-black" href="">Blog</a>
+                </li>
+                <li class="nav-item link">
+                  <a class="nav-link text-black" href="">About Us</a>
+                </li>
+                
+                <li class="nav-item link">
+                  <a class="nav-link bg-danger text-white rounded " href="">Login</a>
+                </li>
+              </ul>
+            </div>
+            
+          </nav>
+
+
+<!--second section!-->
+
+
+
+<div class="container-fluid">
+
+
+<div class="row">
+    <div class="col">
+        <h3></h3>
+        <a href="">Whatsapp Now</a>
+        
+
+
+<ul>
+
+        <li>Instant Reply from our Team</li>
+        <li>Affordable Prices with Quality Work</li>
+        <li>All Subjects Experts From All over the World</li>
+        <li>High Grades</li>
+        </ul>
+    </div>
+    <div class="col">
+        <img src="" alt="images ">
+    </div>
+</div>
+
+
+
+
+</div>
+<!--About Us!-->
+
+<div class="container-fluid mt-3">
+    <h2>Take my Online Class Help for Me</h2>
+
+<p>MasterAssingmentHelp.com is one of the best online platforms to share the burden of students by delivering quality work which help them to rank higher in their studies. If you are also a student who is searching for an online educational assistance, look no further. We, team of professionals and experts of different subjects, are here to help you out, and never let you compromise on your freedom. Therefore, whether you want help in online classes, online courses, exams, assignments, MasterAssignmenthelp’s team will always be there to get you a sigh of relief.  Just contact on the given WhatsApp number and get an optimal solution of your all educational stress.</p>
+<h2>Who We Are?</h2>
+<div class="row">
+    <div class="col">
+        <img src="" alt="">
+        <h2></h2>
+        <a href="">WhatsApp Now!</a>
+    </div>
+    <div class="col">
+
+
+
+    <p>As an Assignment Helper, I have been working on different freelancing platforms for 8 years. In these 8 years, I tried to help students to get high grades in their studies. 
+    </p>
+<p>After serving years in this field, I felt to create an online platform where I can showcase my expertise in a better way. In these 8 years, I managed to build up a team of experts and professionals (Phd and professors of all relevant subjects). You can check all of my work in the sample section. </p>
+    </div>
+</div>
+</div>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
