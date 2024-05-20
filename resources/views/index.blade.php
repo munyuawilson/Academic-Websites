21d41751a519e5d89a3d63463291fa699e2798e4
Ivy Karunjigi <wmunyua4@gmail.com>
2024-05-20 12:04:03 +0300

    progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,21 +11,22 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md navbar-light nav-background fixed-top">
-    <div class=" container-fluid mb-5 justify-content-center ">
-        <ul class="navbar-nav">
-            <li class="nav-item
-            ">+447554912584
+<nav class="navbar navbar-expand-md navbar-light nav-background nav-bg">
+    <div class=" container-fluid justify-content-center ">
+        <ul class="navbar-nav pt-2">
+            <li class="nav-item px-2
+            pt-2">+447554912584
 </li>
-            <li class="nav-item">+923436078969</li>
-            <li class="nav-item">info@masterassignmenthelp.com</li>
-            <li class="nav-item"><a href="">Order On Whatsapp</a></li>
+            <li class="nav-item px-2 pt-2">+923436078969</li>
+            <li class="nav-item px-2 pt-2">info@masterassignmenthelp.com</li>
+            <li class="nav-item px-2 rounded bg-success pt-2 pb-2 "><a href="" class="text-white whatsapp-link" >Order On Whatsapp</a></li>
         </ul>
     </div>
     </nav>
 
-<nav class="navbar navbar-expand-md navbar-light nav-background fixed-top mt-5">
-            <a class="navbar-brand" href=""><img src="/static/logo.png" alt="" height="100px"><a>
+<nav class="navbar navbar-expand-md navbar-light nav-background mt-3 mb-5 pb-5 mx-5">
+  
+            <a class="navbar-brand px-5" href=""><img src="{{ asset('/images\cropped-Untitled-design-2.png') }}" height="70px"  alt="logo"> <a>
             <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -42,11 +43,11 @@
                 </li>
                 
                 <li class="nav-item link">
-                  <a class="nav-link bg-danger text-white rounded " href="">Login</a>
+                  <a class="nav-link bg-danger text-white rounded " href="">Services</a>
                 </li>
               </ul>
-            </div>
             
+            </div>
           </nav>
 
 
@@ -54,26 +55,63 @@
 
 
 
-<div class="container-fluid">
+<div class="container-fluid mt-5">
 
 
-<div class="row">
+<div class="row mt-5">
+
     <div class="col">
-        <h3></h3>
-        <a href="">Whatsapp Now</a>
-        
+      <h2 class="text-center">Master Assignment Help Online for Students</h2>
+        <h3 class="text-center">Your Gateway to Stress-Free Assignments 
+</h3>
+<div class="justify-content-center text-center mt-5">
+        <a href="" class="nav-bg pt-2 pb-2 whatsapp-link rounded text-white px-2">Whatsapp Now</a>
+        </div>
 
 
-<ul>
+<ul class="list justify-content-center mt-3">
 
-        <li>Instant Reply from our Team</li>
-        <li>Affordable Prices with Quality Work</li>
-        <li>All Subjects Experts From All over the World</li>
-        <li>High Grades</li>
+        <li> <span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span> Instant Reply from our Team</li>
+        <li> <span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span> Affordable Prices with Quality Work</li>
+        <li> <span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span> All Subjects Experts From All over the World</li>
+        <li> <span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span> High Grades</li>
         </ul>
     </div>
     <div class="col">
-        <img src="" alt="images ">
+        <!-- Slideshow container -->
+<div class="slideshow-container">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="{{ asset('/images\cropped-Untitled-design-2.png') }}" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="{{ asset('/images\cropped-Untitled-design-2.png') }}" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="{{ asset('/images\cropped-Untitled-design-2.png') }}" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+<span class="dot" onclick="currentSlide(1)"></span>
+<span class="dot" onclick="currentSlide(2)"></span>
+<span class="dot" onclick="currentSlide(3)"></span>
+</div>
     </div>
 </div>
 
@@ -83,32 +121,34 @@
 </div>
 <!--About Us!-->
 
-<div class="container-fluid mt-3">
-    <h2>Take my Online Class Help for Me</h2>
+<div class="container mt-4">
+    <h2 class="text-center text-primary">Take my Online Class Help for Me</h2>
 
-<p>MasterAssingmentHelp.com is one of the best online platforms to share the burden of students by delivering quality work which help them to rank higher in their studies. If you are also a student who is searching for an online educational assistance, look no further. We, team of professionals and experts of different subjects, are here to help you out, and never let you compromise on your freedom. Therefore, whether you want help in online classes, online courses, exams, assignments, MasterAssignmenthelp’s team will always be there to get you a sigh of relief.  Just contact on the given WhatsApp number and get an optimal solution of your all educational stress.</p>
-<h2>Who We Are?</h2>
-<div class="row">
-    <div class="col">
-        <img src="" alt="ceo">
-        <h2>CEO</h2>
-        <a href="">WhatsApp Now!</a>
+<p class="text-center mt-5">MasterAssingmentHelp.com is one of the best online platforms to share the burden of students by delivering quality work which help them to rank higher in their studies. If you are also a student who is searching for an online educational assistance, look no further. We, team of professionals and experts of different subjects, are here to help you out, and never let you compromise on your freedom. Therefore, whether you want help in online classes, online courses, exams, assignments, MasterAssignmenthelp’s team will always be there to get you a sigh of relief.  Just contact on the given WhatsApp number and get an optimal solution of your all educational stress.</p>
+
+
+<h2 class="text-center text-primary mt-5">Who We Are?</h2>
+<div class="row mt-5">
+    <div class="col text-center justify-content-center">
+        <img src="{{ asset('/images/WhatsApp-Image-2023-11-10-at-1.06.22-PM.jpeg') }}" alt="ceo" height="150px" class="ceo-image ">
+        <h4 class="text-primary mt-3 mb-3">CEO MasterAssingmentHelp</h4>
+        <a href=""class="text-white whatsapp-link bg-primary rounded px-2 pb-2 pt-2 mt-5">WhatsApp Now!</a>
     </div>
     <div class="col">
 
 
 
-    <p>As an Assignment Helper, I have been working on different freelancing platforms for 8 years. In these 8 years, I tried to help students to get high grades in their studies. 
+    <p class="font-weight-bold">As an Assignment Helper, I have been working on different freelancing platforms for 8 years. In these 8 years, I tried to help students to get high grades in their studies. 
     </p>
-<p>After serving years in this field, I felt to create an online platform where I can showcase my expertise in a better way. In these 8 years, I managed to build up a team of experts and professionals (Phd and professors of all relevant subjects). You can check all of my work in the sample section. </p>
+<p class="font-weight-bold">After serving years in this field, I felt to create an online platform where I can showcase my expertise in a better way. In these 8 years, I managed to build up a team of experts and professionals (Phd and professors of all relevant subjects). You can check all of my work in the sample section. </p>
    Assignment Writing
-<progress value="70" max="100"></progress>
+<progress value="70" max="100"></progress><br><br>
 Essay Writing
-<progress value="80" max="100"></progress>
+<progress value="80" max="100"></progress><br><br>
 Dissertation Writing
-<progress value="90" max="100">90</progress>
+<progress value="90" max="100">90</progress><br><br>
 Team Management
-<progress value="70" max="100">70</progress>
+<progress value="70" max="100">70</progress> <br><br>
 
 </div>
 </div>
@@ -116,43 +156,50 @@ Team Management
 
 
 
-  <div class="container-fluid">
+  <div class="container-fluid mt-5">
 
-<div class="row">
-<div class="col">
-  <h2>We Offer a Wide Range of Writing Services
+<div class="row bg-primary">
+<div class="col text-center justify-content-center">
+  <h2 class="text-center text-white pt-5">We Offer a Wide Range of Writing Services
 </h2>
 
-<p>Our Assignement Helpers are always up to mitigate the educational stress of students all over the world. You can check our these services or contact us via given WhatsApp and get an instant solution. </p>
+<p class="mt-5 pt-5 text-center text-white">Our Assignement Helpers are always up to mitigate the educational stress of students all over the world. You can check our these services or contact us via given WhatsApp and get an instant solution. </p>
 </div>
-<div class="col"></div>
+<div class="col text-center">
+  <img src="{{ asset('/images/Capture-removebg-preview.png') }}" alt="" class="pt-5 ">
+</div>
 
 </div>
 
 
   </div>  
 
-  <div class="container-fluid mt-3">
-    <h2>Why Choose MasterAssingmentHelp?</h2>
-    <p>Our following features will help you to decide why should you choose us. The following features makes us unique and prominent in the industry and students always prefer MasterAssignmentHelp over others. If you are visiting us first time, just discuss your project on the given Whatsapp, negotiate, and get your task done. Check our following unparalleled characteristics and  decide yourself. </p>
+  <div class="container-fluid mt-5">
+    <h2 class="text-center text-primary">Why Choose MasterAssingmentHelp?</h2>
+    <p class="mt-3">Our following features will help you to decide why should you choose us. The following features makes us unique and prominent in the industry and students always prefer MasterAssignmentHelp over others. If you are visiting us first time, just discuss your project on the given Whatsapp, negotiate, and get your task done. Check our following unparalleled characteristics and  decide yourself. </p>
 
   </div>
-<div class="container">
+
+<div class="container border mt-5">
   <div class="row">
     <div class="col">
-      <ul>
+      <div class="row">
+      <div class="col">
+      <ul class="list">
       
 
 
 
 
-        <li>Instant Reply and Help</li>
-        <li>Plagiarism Free Work</li>
-        <li>No AI written Assignments</li>
-        <li>Experts from All over the World</li>
-        <li>Affordable Prices</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Instant Reply and Help</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Plagiarism Free Work</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>No AI written Assignments</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Experts from All over the World</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Affordable Prices</li>
       </ul>
-      <ul>
+      </div>
+      <div class="col">
+      <ul class="list">
       
       
 
@@ -162,15 +209,17 @@ Team Management
 
 
 
-        <li>100% Money Back Guarantee</li>
-        <li>Meet the Given Timeline</li>
-        <li>Work until Satisfaction</li>
-        <li>Projects, Assignments, Course</li>
-        <li>Familiar with all types of Web Portals</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>100% Money Back Guarantee</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Meet the Given Timeline</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Work until Satisfaction</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Projects, Assignments, Course</li>
+        <li><span class="text-primary" style="font-size: 24px; font-weight: bold;">&#10003;</span>Familiar with all types of Web Portals</li>
       </ul>
+      </div>
+      </div>
     </div>
-    <div class="col">
-      <img src="" alt="img">
+    <div class="col text-center">
+      <img src="{{ asset('/images/6d897e8c-2bff-4237-8e9d-a57620e64fcb.jpg') }}" alt="img" height="200px">
     </div>
   </div>
 </div>
@@ -178,19 +227,57 @@ Team Management
 
 
 
-<div class="container">
-  <h2>Assignment Help Online for Students</h2>
-  <div class="row">
-    <div class="col"><img src="" alt="img"></div>
+<div class="container mt-5">
+  <h2 class="text-center text-primary">Assignment Help Online for Students</h2>
+  <div class="row mt-5">
+    <div class="col text-center"><img src="{{ asset('/images/ea0d0941-66b1-49ae-9686-07209f8ed3c6-792x1024-removebg-preview.png') }}" alt="img" height="300px" class="text-center"></div>
     <div class="col">
 
 
     <p>In today advanced era, we are supposed to manage dozens of tasks daily. For students, it is a hard nut to crack to manage their routine work as they are likely to be engaged in different activities. Moreover, various students have to do part time job to meet their expenses as well.  Various responsibilities on students drain their energy. Therefore, students have little time to enjoy after spending hours of their time in colleges and universities. 
 
-However, MasterAssignmentHelp is always there to help students by mitigating their load. We get you covered in assignment writings, essay writing, online classes, online complete courses, online exams help, and projects. Our team of experts of all subjects will help you to get high grades in all the given tasks. So, don’t compromise on your freedom and share your burden with us. Be smart and just contact us on the given WhatsApp and discuss your tasks with us. </p>
-    </div>
+<p>However, MasterAssignmentHelp is always there to help students by mitigating their load. We get you covered in assignment writings, essay writing, online classes, online complete courses, online exams help, and projects. Our team of experts of all subjects will help you to get high grades in all the given tasks. So, don’t compromise on your freedom and share your burden with us. Be smart and just contact us on the given WhatsApp and discuss your tasks with us. </p>
+</p> </div>
   </div>
 </div>
+
+
+
+<h2 class="text-center text-primary mt-5 ">How does it work</h2>
+
+<div class="container-fluid mt-5 border">
+
+
+
+<div class="row mt-5">
+  <div class="col text-center">
+
+ <h4 class="text-center text-primary">Contact Us on WhatsApp</h4> 
+<p>For the sake of your convenience, our Team is available 24/7. You can contact us any time on WhatsApp.  Send us a message with relevant query.  
+
+</p>
+  </div>
+  <div class="col text-center">
+ <h4 class="text-center text-primary">Place your Order</h4> 
+<p>After finalizing the deal, place your customized order. Pay the charges via PayPal, Wise, or Payoneer. </p>
+
+
+  </div>
+
+</div>
+<div class="row mt-5">
+  <div class="col text-center"><h4 class="text-center text-primary">Discuss Your Task</h4>
+<p>After Getting instant reply from our team, discuss your project/assignment with our team. Inform us about your demands. 
+</p>
+</div>
+  <div class="col text-center"><h4 class="text-center text-primary">Sit Back, Wait, Enjoy</h4>
+<p>Your task is over. Just sit back, wait and enjoy your life. Before deadline, you will get your quality work as per discussion. </div>
+</p>
+</div>
+</div>
+
+
 </body>
+<script src="{{ asset('/js/index.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
